6c99c08e6bc0ab8b3437122091a5927497cfd7c6
sequielo <dev@sequielo.com.ar>
2013-06-21 03:35:57 -0300

    improved tests.sh

--- tests.sh
+++ tests.sh
@@ -1,31 +1,45 @@
 #!/bin/bash
-N=500
+N=400
 T=10
 
 MEM=$(echo "$N*$N*4*3/1024/1024" | bc -l | awk '{printf "%08f\n", $0}')
+
+make
+
 echo ""
-echo "==== Matrix size: ${N}x${N}"
-echo "---- Trip Count:  $T"
-echo "---- Memory:      $MEM MB"
+echo "==== Matrices size: ${N}x${N}"
+echo "---- Trip Count:    $T"
+echo "---- Memory Used:   $MEM MB"
 echo ""
-echo "!! Make CPU use Performance Governor !!"
+echo "!! Make CPU use Performance Governor for testing !!"
+echo ""
 echo "-- Press Enter to Begin..."
 read
 
 export CL_HELPER_NO_COMPILER_OUTPUT_NAG=1
 
 echo ""
-echo "----- Traditional Multiplication:"
+echo "----- 1) Traditional Multiplication:"
 bin/cl-mul0 $N $T
 
 echo ""
-echo "----- Multiplication with SAXPY:"
+echo "----- 2) Multiplication using SAXPY:"
 bin/cl-mul0-saxpy $N $T
 
 echo ""
-echo "----- OpenCL Traditional Multiplication:"
+echo "----- 3) Multiplication using Blocks:"
+echo "TODO"
+#bin/cl-mul0-blocks $N $T
+
+echo ""
+echo "----- 4) OpenCL Traditional Multiplication:"
 bin/cl-mul1 $N $T
 
 echo ""
-echo "----- OpenCL SAXPY Multiplication:"
-bin/cl-mul2 $N $T
+echo "----- 5) OpenCL Multiplication using SAXPY:"
+echo "TODO"
+#bin/cl-mul2 $N $T
+
+echo ""
+echo "----- 6) OpenCL Multiplication using Blocks:"
+bin/cl-mul3-blocks $N $T
