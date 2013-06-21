85a83c9512bfc8afee57e6bf0a627e997f7ed7d6
sequielo <dev@sequielo.com.ar>
2013-06-21 17:45:24 -0300

    updated print-devices for all plataforms

--- print-devices.c
+++ print-devices.c
@@ -3,5 +3,11 @@
 int main(int argc, char **argv)
 {
   print_platforms_devices();
+
+  cl_context ctx;
+  cl_command_queue queue;
+  create_context_on(CHOOSE_INTERACTIVELY, CHOOSE_INTERACTIVELY, 0, &ctx, &queue, 0);
+  print_device_info_from_queue(queue);
+
   return 0;
 }
